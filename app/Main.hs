4ec08f95d7399075473429cc2bbd58b154aeda06
Kacper Grabczak <kacpergrabczak@gmail.com>
2026-02-28 18:30:44 +0100

    Add parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import Lib (fromJSON, toJSON)
+import Text.Megaparsec (errorBundlePretty)
 
 main :: IO ()
-main = someFunc
+main = do
+  print "Path to JSON file:"
+  path <- getLine
+  contents <- readFile path
+  print "File contents:"
+  print contents
+  case fromJSON contents of
+    Left err -> print $ "Error parsing JSON: " ++ errorBundlePretty err
+    Right jvalue -> do
+      print "Parsed JSON value:"
+      print jvalue
+      print "Stringified back to JSON:"
+      print $ toJSON jvalue
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,94 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib (JValue (..), fromJSON, toJSON) where
+
+import Data.List (intercalate)
+import Data.Scientific (Scientific)
+import Data.Text (Text, pack)
+import Data.Void (Void)
+import Text.Megaparsec (MonadParsec (eof), ParseErrorBundle, Parsec, choice, manyTill, parse, sepBy)
+import Text.Megaparsec.Char (char, string)
+import Text.Megaparsec.Char.Lexer (charLiteral, scientific, signed)
+
+type Parser = Parsec Void Text
+
+data JValue
+  = JNull
+  | JBool Bool
+  | JNumber Scientific
+  | JString String
+  | JArray [JValue]
+  | JObject [(String, JValue)]
+  deriving (Eq, Show)
+
+jsonScheme :: Parser JValue
+jsonScheme =
+  choice
+    [ jsonNull,
+      jsonBool,
+      jsonNumber,
+      jsonString,
+      jsonArray,
+      jsonObject
+    ]
+
+jsonNull :: Parser JValue
+jsonNull = do
+  _ <- string "null"
+  pure JNull
+
+jsonBool :: Parser JValue
+jsonBool = do
+  b <- choice [string "true", string "false"]
+  pure $ JBool (b == "true")
+
+jsonNumber :: Parser JValue
+jsonNumber = do
+  n <- signed (pure ()) scientific
+  pure $ JNumber n
+
+jsonString :: Parser JValue
+jsonString = do
+  _ <- char '"'
+  content <- manyTill charLiteral (char '"')
+  pure $ JString content
+
+jsonArray :: Parser JValue
+jsonArray = do
+  _ <- char '['
+  values <- jsonScheme `sepBy` char ','
+  _ <- char ']'
+  pure $ JArray values
+
+jsonPair :: Parser (String, JValue)
+jsonPair = do
+  (JString key) <- jsonString
+  _ <- char ':'
+  value <- jsonScheme
+  pure (key, value)
+
+jsonObject :: Parser JValue
+jsonObject = do
+  _ <- char '{'
+  pairs <- jsonPair `sepBy` char ','
+  _ <- char '}'
+  pure $ JObject pairs
+
+jsonValue :: Parser JValue
+jsonValue = do
+  value <- jsonScheme
+  eof
+  pure value
+
+fromJSON :: String -> Either (ParseErrorBundle Text Void) JValue
+fromJSON input = parse jsonValue "" (pack input)
+
+toJSON :: JValue -> String
+toJSON input = case input of
+  JNull -> "null"
+  JBool True -> "true"
+  JBool False -> "false"
+  JNumber n -> show n
+  JString s -> "\"" ++ s ++ "\""
+  JArray arr -> "[" ++ intercalate "," (map toJSON arr) ++ "]"
+  JObject obj -> "{" ++ intercalate "," (map (\(k, v) -> "\"" ++ k ++ "\":" ++ toJSON v) obj) ++ "}"
